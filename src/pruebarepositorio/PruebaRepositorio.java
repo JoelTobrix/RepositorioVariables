785f910d8265284d27ee462b392d3c9d5d910e13
PERSONAL <PERSONAL@PC>
2024-02-05 21:41:26 -0500

    Mostrando archivos en el repositorio

---
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebarepositorio;
+
+import java.util.Scanner;
+
+
+public class PruebaRepositorio {
+
+    
+    public static void main(String[] args) {
+        // Meotodo main
+        Scanner dato = new Scanner(System.in);
+     int num1,num2;
+     
+        
+        
+    }
+    
+}
